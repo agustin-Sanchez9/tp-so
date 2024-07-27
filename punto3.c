7d325593f1a9acc6eac28500f05c8221ca3999c9
agustin-Sanchez9 <agustinsanchezp123@gmail.com>
2024-07-27 19:03:25 -0300

    update

---
+++ punto3.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+/* para simular la seccion critica los hilos escribiran en consola la letra correspodiente a cada uno
+y modificaran una variable, siendo que el hilo A sumara 1 y los hilos B y C restaran 1 
+de forma que la variable debera terminar con el mismo valor con le que comienza (10). */
+
+// creo var global
+int var = 10;
+
+
+// incio los mutex para cada hilo
+pthread_mutex_t mA = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mB = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mC = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mX = PTHREAD_MUTEX_INITIALIZER; 
+
+// rutinas de los hilos
+void *rutina_hiloA(void *arg){
+    pthread_mutex_lock(&mA);
+    printf("A");
+    var++;
+    pthread_mutex_unlock(&mX);
+    return 0;
+}
+
+void *rutina_hiloB(void *arg){
+    pthread_mutex_lock(&mB);
+    pthread_mutex_lock(&mX);
+    printf("B");
+    var--;
+    pthread_mutex_unlock(&mA);
+    pthread_mutex_unlock(&mC);
+    return 0;
+
+}
+
+void *rutina_hiloC(void *arg){
+    pthread_mutex_lock(&mC);
+    pthread_mutex_lock(&mX);
+    printf("C");
+    var--;
+    pthread_mutex_unlock(&mA);
+    pthread_mutex_unlock(&mB);
+    return 0;
+}
+
+
+
+int main(void){
+    
+    pthread_t hiloA, hiloB, hiloC;
+
+    // cierro el mutex C y X
+    // mA = 1 ; mB = 1 ; mC = 0 ; mX = 0
+    pthread_mutex_lock(&mC);
+    pthread_mutex_lock(&mX);
+
+    // creacion de hilos
+    pthread_create(&hiloA,NULL,rutina_hiloA,NULL);
+    pthread_create(&hiloB,NULL,rutina_hiloB,NULL);
+    pthread_create(&hiloC,NULL,rutina_hiloC,NULL);
+
+    pthread_join(hiloA,NULL);
+    pthread_join(hiloB,NULL);
+    pthread_join(hiloC,NULL);
+
+    return 0;
+}
