846b620a2d238c25bb12cc3f2d9626214e9524a2
agustin-Sanchez9 <agustinsanchezp123@gmail.com>
2024-09-09 19:01:46 -0300

    update

--- punto7.c
+++ punto7.c
@@ -1,106 +1,140 @@
-#include <unistd.h>  
-#include <netdb.h> 
-#include <netinet/in.h> 
-#include <sys/socket.h> 
-#include <sys/types.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <errno.h>
-#include <stdio.h> 
-#include <string.h> 
+#include <sys/wait.h>
 
-// parametros del server
-#define SERV_PORT       8080              // puerto
-#define SERV_HOST_ADDR "192.168.0.21"     // IP
-#define BUF_SIZE        100               // buffer para rx y tx
-#define BACKLOG         5                 //
+#define PORT 8080
+#define BUFFER_SIZE 1024
 
-int main(int argc, char* argv[])          
-{ 
-    int sockfd, connfd ;  /* listening socket and connection socket file descriptors */
-    unsigned int len;     /* length of client address */
-    struct sockaddr_in servaddr, client; 
-    
-    int  len_rx, len_tx = 0;                     /* received and sent length, in bytes */
-    char buff_tx[BUF_SIZE] = "Hello client, I am the server";
-    char buff_rx[BUF_SIZE];   /* buffers for reception  */
-    
-     
-    // creacion del socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sockfd == -1) 
-    { 
-        fprintf(stderr, "[SERVER-error]: socket creation failed. %d: %s \n", errno, strerror( errno ));
-        return -1;
-    } 
-    else
-    {
-        printf("[SERVER]: Socket successfully created..\n"); 
+void clean_command(char *command);
+void *handle_client(void *client_socket);
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    // Crear el socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
     }
-    
-    memset(&servaddr, 0, sizeof(servaddr));
-  
-    /* assign IP, SERV_PORT, IPV4 */
-    servaddr.sin_family      = AF_INET; 
-    servaddr.sin_addr.s_addr = inet_addr(SERV_HOST_ADDR); 
-    servaddr.sin_port        = htons(SERV_PORT); 
-    
-    
-    /* Bind socket */
-    if ((bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) 
-    { 
-        fprintf(stderr, "[SERVER-error]: socket bind failed. %d: %s \n", errno, strerror( errno ));
-        return -1;
-    } 
-    else
-    {
-        printf("[SERVER]: Socket successfully binded \n");
+
+    // Definir la dirección del servidor
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Enlazar el socket
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
     }
-  
-    /* Listen */
-    if ((listen(sockfd, BACKLOG)) != 0) 
-    { 
-        fprintf(stderr, "[SERVER-error]: socket listen failed. %d: %s \n", errno, strerror( errno ));
-        return -1;
-    } 
-    else
-    {
-        printf("[SERVER]: Listening on SERV_PORT %d \n\n", ntohs(servaddr.sin_port) ); 
+
+    // Escuchar conexiones entrantes
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
     }
-    
-    len = sizeof(client); 
-  
-      /* Accept the data from incoming sockets in a iterative way */
-      while(1)
-      {
-        connfd = accept(sockfd, (struct sockaddr *)&client, &len); 
-        if (connfd < 0) 
-        { 
-            fprintf(stderr, "[SERVER-error]: connection not accepted. %d: %s \n", errno, strerror( errno ));
-            return -1;
-        } 
-        else
-        {              
-            while(1) /* read data from a client socket till it is closed */ 
-            {  
-                /* read client message, copy it into buffer */
-                len_rx = read(connfd, buff_rx, sizeof(buff_rx));  
-                
-                if(len_rx == -1)
-                {
-                    fprintf(stderr, "[SERVER-error]: connfd cannot be read. %d: %s \n", errno, strerror( errno ));
-                }
-                else if(len_rx == 0) /* if length is 0 client socket closed, then exit */
-                {
-                    printf("[SERVER]: client socket closed \n\n");
-                    close(connfd);
-                    break; 
-                }
-                else
-                {
-                    write(connfd, buff_tx, strlen(buff_tx));
-                    printf("[SERVER]: %s \n", buff_rx);
-                }            
-            }  
-        }                      
-    }    
-} 
+
+    printf("Servidor esperando conexiones...\n");
+
+    while (1) {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Conexión aceptada\n");
+
+        // Crear un nuevo hilo para manejar el cliente
+        pthread_t thread_id;
+        int *client_socket = malloc(sizeof(int));
+        *client_socket = new_socket;
+
+        if (pthread_create(&thread_id, NULL, handle_client, (void *)client_socket) != 0) {
+            perror("pthread_create");
+            exit(EXIT_FAILURE);
+        }
+
+        // Detach the thread to free resources when it terminates
+        pthread_detach(thread_id);
+    }
+
+    return 0;
+}
+
+
+void clean_command(char *command) {
+    int len = strlen(command);
+    for (int i = 0; i < len; i++) {
+        if (command[i] == '\r' || command[i] == '\n') {
+            command[i] = '\0';
+        }
+    }
+}
+
+void *handle_client(void *client_socket) {
+    int new_socket = *((int *)client_socket);
+    free(client_socket);
+
+    char buffer[BUFFER_SIZE];
+    int bytes_read;
+
+    while ((bytes_read = read(new_socket, buffer, sizeof(buffer) - 1)) > 0) {
+        buffer[bytes_read] = '\0';
+
+        clean_command(buffer);  // Limpiar el comando
+
+        if (strcmp(buffer, "salir") == 0) {
+            printf("Cliente desconectado\n");
+            break;
+        }
+
+        printf("Comando recibido: %s\n", buffer);
+
+        int pipefd[2];
+        if (pipe(pipefd) == -1) {
+            perror("pipe");
+            exit(EXIT_FAILURE);
+        }
+
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid == 0) {
+            // Proceso hijo
+            close(pipefd[0]); // Cerrar extremo de lectura
+
+            // Redirigir salida estándar al pipe
+            dup2(pipefd[1], STDOUT_FILENO);
+            close(pipefd[1]);
+
+            char *args[] = {"/bin/sh", "-c", buffer, NULL};
+            execvp(args[0], args);
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        } else {
+            // Proceso padre
+            close(pipefd[1]); // Cerrar extremo de escritura
+            char result[BUFFER_SIZE];
+            int read_bytes;
+            while ((read_bytes = read(pipefd[0], result, sizeof(result) - 1)) > 0) {
+                result[read_bytes] = '\0';
+                send(new_socket, result, read_bytes, 0);
+            }
+            close(pipefd[0]);
+            wait(NULL);
+        }
+    }
+
+    close(new_socket);
+    pthread_exit(NULL);
+}
